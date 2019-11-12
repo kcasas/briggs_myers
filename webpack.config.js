1e1af032f6d18ca630cf5df5a61fef5747098a84
kcasas <karl.casas777@gmail.com>
2019-11-12 10:45:25 +0800

    webpack build output to docs for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name].[contenthash].js'
   },
   module: {
